e6c80a936ffdf6fd8e3965020e02688946433137
lele0015 <lucas.leleur@etudiant.univ-reims.fr>
2024-08-07 16:14:34 +0200

    Ajout effet visuel page actuelle navbar

--- app/ui/aboutme/navbar.tsx
+++ app/ui/aboutme/navbar.tsx
@@ -2,6 +2,8 @@
 
 import { motion } from 'framer-motion';
 import Link from 'next/link';
+import { clsx } from 'clsx';
+import { usePathname } from 'next/navigation';
 
 const container = {
     hidden: { 
@@ -29,9 +31,10 @@ const child = {
 }
 
 const Navbar = () => {
+    const pathname = usePathname();
     return (
         <motion.div
-                className="w-60 flex flex-col bg-blue-950 p-8 text-white"
+                className="w-72 flex flex-col bg-blue-950 p-8 text-white"
                 variants={container}
                 initial="hidden"
                 animate="visible"
@@ -42,14 +45,14 @@ const Navbar = () => {
                  variants={child}
                 >
                     <Link href="/">
-                    <img src={"../logo.png"} className="bg-white rounded-full"></img>
+                        <img src={"../logo.png"} className="bg-white rounded-full"></img>
                     </Link>
                 </motion.span>
                 <motion.span variants={child}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                         <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
                     </svg>
-                    <Link href="/aboutme/profile">Profil</Link>
+                    <Link className={clsx({'bg-white text-blue-950 pl-4 pr-4 rounded-full' : pathname == '/aboutme/profile'})} href="/aboutme/profile">Profil</Link>
                 </motion.span>
                 <motion.span variants={child}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
@@ -58,24 +61,24 @@ const Navbar = () => {
                         <path d="M12 16.5c2.685 0 5.19-.586 7.078-1.609a8.282 8.282 0 0 0 1.897-1.384c.016.121.025.244.025.368 0 2.692-4.03 4.875-9 4.875s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.284 8.284 0 0 0 1.897 1.384C6.809 15.914 9.315 16.5 12 16.5Z" />
                         <path d="M12 20.25c2.685 0 5.19-.586 7.078-1.609a8.282 8.282 0 0 0 1.897-1.384c.016.121.025.244.025.368 0 2.692-4.03 4.875-9 4.875s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.284 8.284 0 0 0 1.897 1.384C6.809 19.664 9.315 20.25 12 20.25Z" />
                     </svg>
-                    <Link href="/aboutme/skills">Compétences</Link>
+                    <Link className={clsx({'bg-white text-blue-950 pl-4 pr-4 rounded-full' : pathname == '/aboutme/skills'})} href="/aboutme/skills">Compétences</Link>
                 </motion.span>
                 <motion.span variants={child}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                         <path fillRule="evenodd" d="M7.5 5.25a3 3 0 0 1 3-3h3a3 3 0 0 1 3 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0 1 12 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 0 1 7.5 5.455V5.25Zm7.5 0v.09a49.488 49.488 0 0 0-6 0v-.09a1.5 1.5 0 0 1 1.5-1.5h3a1.5 1.5 0 0 1 1.5 1.5Zm-3 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
                         <path d="M3 18.4v-2.796a4.3 4.3 0 0 0 .713.31A26.226 26.226 0 0 0 12 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 0 1-6.477-.427C4.047 21.128 3 19.852 3 18.4Z" />
                     </svg>
-                    <Link href="/aboutme/experiences">Expériences</Link>  
+                    <Link className={clsx({'bg-white text-blue-950 pl-4 pr-4 rounded-full' : pathname == '/aboutme/experiences'})} href="/aboutme/experiences">Expériences</Link>  
                 </motion.span> 
                 <motion.span variants={child}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                         <path d="M12 .75a8.25 8.25 0 0 0-4.135 15.39c.686.398 1.115 1.008 1.134 1.623a.75.75 0 0 0 .577.706c.352.083.71.148 1.074.195.323.041.6-.218.6-.544v-4.661a6.714 6.714 0 0 1-.937-.171.75.75 0 1 1 .374-1.453 5.261 5.261 0 0 0 2.626 0 .75.75 0 1 1 .374 1.452 6.712 6.712 0 0 1-.937.172v4.66c0 .327.277.586.6.545.364-.047.722-.112 1.074-.195a.75.75 0 0 0 .577-.706c.02-.615.448-1.225 1.134-1.623A8.25 8.25 0 0 0 12 .75Z" />
                         <path fillRule="evenodd" d="M9.013 19.9a.75.75 0 0 1 .877-.597 11.319 11.319 0 0 0 4.22 0 .75.75 0 1 1 .28 1.473 12.819 12.819 0 0 1-4.78 0 .75.75 0 0 1-.597-.876ZM9.754 22.344a.75.75 0 0 1 .824-.668 13.682 13.682 0 0 0 2.844 0 .75.75 0 1 1 .156 1.492 15.156 15.156 0 0 1-3.156 0 .75.75 0 0 1-.668-.824Z" clipRule="evenodd" />
                     </svg>
-                    <Link href="/aboutme/projects">Projets</Link>  
+                    <Link className={clsx({'bg-white text-blue-950 pl-4 pr-4 rounded-full' : pathname == '/aboutme/projects'})} href="/aboutme/projects">Projets</Link>  
                 </motion.span>
                 <div className="flex grow flex-col justify-end">
-                    <Link href="/aboutme/contact" className="bg-indigo-900 p-2 text-center text-xl font-bold">Contact</Link>
+                    <Link className={clsx('bg-indigo-900 p-2 text-center text-xl font-bold',{'bg-white text-blue-950 pl-4 pr-4 rounded-full' : pathname == '/aboutme/contact'})} href="/aboutme/contact">Contact</Link>
                     <div className="flex justify-between">
                         <a href="https://www.linkedin.com/in/lucas-leleu-bb24151b9/"><img src="../Linkedin.png"></img></a>
                         <a href="https://www.instagram.com/l.lucass_/"><img src="../Instagram.png"></img></a>
